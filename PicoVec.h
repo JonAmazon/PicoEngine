289e41e346f83ec1c4da03e5a4b6d4befe3fb86d
JonAmazon <sir.JAmazon@gmail.com>
2014-11-19 13:18:56 -0500

    Create PicoVec.h

---
+++ PicoVec.h
@@ -0,0 +1,35 @@
+#ifndef _H_PICOVEC
+#define _H_PICOVEC
+
+#include<cstdio>
+#include<cstdlib>
+#include<cmath>
+
+struct PicoVec4f
+{
+    float x,y,z,w;
+
+    PicoVec4f();
+    PicoVec4f(float xin,float yin,float zin,float win);
+    
+    float lengthSqrd();
+    float length();
+    
+    void zero();
+    void operator=(const PicoVec4f& operand);
+    void operator+=(const PicoVec4f& operand);
+    void operator-=(const PicoVec4f& operand);
+    void operator*=(const float& scalar);
+    void operator/=(const float& scalar);
+
+    PicoVec4f operator-();
+    PicoVec4f operator+(const PicoVec4f& operand);
+    PicoVec4f operator-(const PicoVec4f& operand);
+    PicoVec4f operator*(const float& scalar);
+    PicoVec4f operator/(const float& scalar);
+    float innerProduct(const PicoVec4f& operand);
+    PicoVec4f crossProduct(const PicoVec4f& operand);
+    PicoVec4f directProduct(const PicoVec4f& operand);
+};
+
+#endif
